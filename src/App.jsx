35ae517bfef18551f27a7fcdf7de710d6c691d05
Olatayo Tijesu <tjayolatayo@gmail.com>
2023-08-07 17:05:51 +0000

    Add verification page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import LandingPage from "./pages/LandingPage"
 import SignUp from "./pages/signup-login/SignUp"
+import Verification from "./pages/signup-login/Verification"
 
 function App() {
   return (
     <>
       {/* <LandingPage/> */}
-      <SignUp/>
+      {/* <SignUp/> */}
+      <Verification/>
     </>
   )
 }
---
+++ src/pages/signup-login/Verification.jsx
@@ -0,0 +1,24 @@
+const Verification = () => {
+  return ( <div className="container verificationPage">
+    <header>
+      <nav>
+        <p><span className="white-line">Me</span>dli<span className="red-line">nk</span></p> 
+      </nav>
+    </header>
+    <main>
+      <h1>Verify your Email</h1>
+      <p>An <span className="OTP">OTP</span> has been sent to your email. Enter the <span className="OTP">OTP</span> to verify your email</p>
+      <form className="verify">
+        <div className="otp">
+          <input type="text" id="" />
+          <input type="text" id="" />
+          <input type="text" id="" />
+          <input type="text" id="" />
+        </div>
+        <button>Next</button>
+      </form>
+    </main>
+  </div> );
+}
+ 
+export default Verification;
