04f8835054382845f6c88e9f50dd24e945430140
Olatayo Tijesu <tjayolatayo@gmail.com>
2023-08-07 16:18:25 +0000

    Add signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import LandingPage from "./pages/LandingPage"
+import SignUp from "./pages/signup-login/SignUp"
 
 function App() {
   return (
     <>
-      <LandingPage/>
+      {/* <LandingPage/> */}
+      <SignUp/>
     </>
   )
 }
---
+++ src/pages/signup-login/SignUp.jsx
@@ -0,0 +1,63 @@
+const SignUp = () => {
+  return ( 
+    <div className="container signUpPage">
+      <header>
+        <nav>
+          <p><span className="white-line">Me</span>dli<span className="red-line">nk</span></p> 
+        </nav>
+      </header>
+      <main>
+        <h1>Sign Up</h1>
+        <form>
+          <div className="signupForm">
+            <div className="firstForm">
+              <input
+                type="text"
+                placeholder="First Name"
+              />
+              <input
+                type="email"
+                placeholder="Email Address"
+              />
+              <input
+                type="text"
+                placeholder="NHIS"
+              />
+              <select id="gender">
+                <option value="">Select Gender</option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+              </select>
+            </div>
+            <div className="secondForm">
+              <input
+                type="text"
+                placeholder="Last Name"
+              />
+              <input
+                type="number"
+                placeholder="Mobile Number"
+              />
+              <input
+                type="date"
+                placeholder="Date of Birth"
+              />
+              <input
+                type="password"
+                placeholder="Password"
+              />
+            </div>
+          </div>
+          <input 
+            type="password" 
+            className="confirm-password"
+            placeholder="Confirm Password" 
+          />
+          <button>Register</button>
+        </form>
+      </main>
+    </div> 
+  );
+}
+ 
+export default SignUp;
