767926c7a4d6ce1d17ad28aa4af5d5edd767eb32
Olatayo Tijesu <tjayolatayo@gmail.com>
2023-08-09 16:07:01 +0100

    Complete navbar for mobile

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -5,7 +5,10 @@ import logo from './../assets/icons/logo2.svg';
 import blocks from './../assets/icons/blocks.svg';
 import chart from './../assets/icons/chart.svg';
 import chat from './../assets/icons/chat.svg';
-import close from './../assets/icons/close.png';
+import home from './../assets/icons/home.svg';
+import appointmentIcon from './../assets/icons/app-icon.svg';
+import notifyIcon from './../assets/icons/note-nav.svg';
+import record from './../assets/icons/record.svg';
 
 const NavBar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -35,10 +38,10 @@ const NavBar = () => {
         <nav className='header'>
           <ul className='leftNav'>
             <li><img src={logo} alt="Logo" /></li>
-            <li className='noShow'><NavLink>Home</NavLink></li>
-            <li className='noShow'><NavLink>Appointments</NavLink></li>
-            <li className='noShow'><NavLink>Notifications</NavLink></li>
-            <li className='noShow'><NavLink>Medical History</NavLink></li>
+            <li className='noShow'><NavLink to='/home/'>Home</NavLink></li>
+            <li className='noShow'><NavLink to='Appointments'>Appointments</NavLink></li>
+            <li className='noShow'><NavLink to='Notifications'>Notifications</NavLink></li>
+            <li className='noShow'><NavLink to='MedHistory'>Medical History</NavLink></li>
           </ul>
           <ul className='rightNav'>
             <li><img src={blocks} alt="" /></li>
@@ -64,17 +67,25 @@ const NavBar = () => {
             </div>
           </li>
         ) : (
-            <p>Home</p> 
+            <p className='header'>Home</p> 
         )}
       </div>
       {isMobile && (
         <aside>
           <nav className={`secondNav ${isMenuOpen ? 'open' : ''}`}> 
             <ul>
-              <li><Link>Home</Link></li>
-              <li><Link>Appointments</Link></li>
-              <li><Link>Notifications</Link></li>
-              <li><Link>Medical History</Link></li>
+              <li>
+                <img src={home} alt="" />
+                <Link to='/home/'>Home</Link></li>
+              <li>
+                <img src={appointmentIcon} alt="" />
+                <Link to='Appointments'>Appointments</Link></li>
+              <li>
+                <img src={notifyIcon} alt="" />
+                <Link to='Notifications'>Notifications</Link></li>
+              <li>
+                <img src={record} alt="" />
+                <Link to='MedHistory'>Medical History</Link></li>
             </ul>
           </nav>
         </aside>
