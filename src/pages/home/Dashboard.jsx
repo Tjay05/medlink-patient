13a04c747fc4b50d673e92278d6b358ebc2ef6b4
Olatayo Tijesu <tjayolatayo@gmail.com>
2023-08-10 21:55:34 +0100

    Import react calendar and complete desktop style for the dashboard

--- src/pages/home/Dashboard.jsx
+++ src/pages/home/Dashboard.jsx
@@ -2,76 +2,83 @@ import appointmentIcon  from '../../assets/icons/today-appt.svg';
 import docIcon from '../../assets/icons/available-doc.svg'
 import clockIcon from '../../assets/icons/clock.svg';
 import profilePic from '../../assets/images/doc1.png';
+import Calendar from 'react-calendar';
 
 const Dashboard = () => {
   return ( 
     <div className="wrapper">
-    <div className="doctor-dashboard">
-      <div className="grid-container">
-        <article className="welcome">
-          <div className="welcomeWrap">
-            <p className="text-wrapper">Welcome</p>
-            <p className="userName">Lois Poppins</p>
-            <div className="welcome-line"></div>
-            <p className="tip">An apple a day, keeps the doctors away. Eat an apple today!!!</p>
-          </div>
-        </article>
-        <section className="cta">
-          <div className="appToday">
-            <img src={appointmentIcon} alt="" />
-            <div className="appoint-details">
-              <p className='num'>0</p>
-              <p className='appt'>Appointments</p>
-              <p className='today'>Today</p>
-            </div>
-          </div>
-          <div className="dividerLine"></div>
-          <div className="appToday">
-            <img src={docIcon} alt="" />
-            <div className="appoint-details">
-              <p className='num'>30</p>
-              <p className='appt'>Available Doctors</p>
-              <p className='today'>Today</p>
-            </div>
-          </div>
-        </section> 
-      </div>
-      <section className="recentDoctors">
-        <h2>Today's Doctors</h2>
-        <div className="docsCard">
-          <article>
-            <div className="top">
-              <img src={profilePic} alt="Profile" />
-              <p className='docName'>Dr. Jacob Jones</p>
-              <p className='role'>General Doctor</p>
-            </div>
-            <div className="bottom">
-              <img src={clockIcon} alt="Profile" /> 
-              <p>9:00 am to 2:30 pm</p>
+      <div className="doctor-dashboard">
+        <div className="grid-container">
+          <article className="welcome">
+            <div className="welcomeWrap">
+              <p className="text-wrapper">Welcome,</p>
+              <p className="userName">Lois Poppins</p>
+              <div className="welcome-line"></div>
+              <p className="tip">An apple a day, keeps the doctors away. Eat an apple today!!!</p>
             </div>
           </article>
-          <article>
-            <div className="top">
-              <img src={profilePic} alt="Profile" />
-              <p className='docName'>Dr. Jacob Jones</p>
-              <p className='role'>General Doctor</p>
+          <section className="cta">
+            <div className="appToday">
+              <img src={appointmentIcon} alt="" />
+              <div className="appoint-details">
+                <p className='num'>0</p>
+                <p className='appt'>Appointments</p>
+                <p className='today'>Today</p>
+              </div>
             </div>
-            <div className="bottom">
-              <img src={clockIcon} alt="Profile" /> 
-              <p>9:00 am to 2:30 pm</p>
+            <div className="dividerLine"></div>
+            <div className="appToday">
+              <img src={docIcon} alt="" />
+              <div className="appoint-details">
+                <p className='num'>30</p>
+                <p className='appt'>Available Doctors</p>
+                <p className='today'>Today</p>
+              </div>
             </div>
-          </article>
+          </section> 
         </div>
-      </section>
-      <aside className="missedApp">
-        <button>Missed your Appointment?</button>
-        <a href="#">Reschedule your Appointment</a>
-      </aside>
-    </div>
+        <section className="recentDoctors">
+          <h2>Today's Doctors</h2>
+          <div className="docsCard">
+            <article>
+              <div className="top">
+                <img src={profilePic} alt="Profile" />
+                <p className='docName'>Dr. Jacob Jones</p>
+                <p className='role'>General Doctor</p>
+              </div>
+              <div className="bottom">
+                <img src={clockIcon} alt="Profile" /> 
+                <p>9:00 am to 2:30 pm</p>
+              </div>
+            </article>
+            <article>
+              <div className="top">
+                <img src={profilePic} alt="Profile" />
+                <p className='docName'>Dr. Jacob Jones</p>
+                <p className='role'>General Doctor</p>
+              </div>
+              <div className="bottom">
+                <img src={clockIcon} alt="Profile" /> 
+                <p>9:00 am to 2:30 pm</p>
+              </div>
+            </article>
+          </div>
+        </section>
+        <aside className="missedApp">
+          <button>Missed your Appointment?</button>
+          <a href="#">Reschedule your Appointment</a>
+        </aside>
+      </div>
+      <div className="centerLine"></div>
+      <div className="calendarWrap">
+        <Calendar />
+        <aside className="missedApp">
+          <button>Missed your Appointment?</button>
+          <a href="#">Reschedule your Appointment</a>
+        </aside>
+      </div>    
     </div>
    );
 }
  
 export default Dashboard;
-
-// 2526
