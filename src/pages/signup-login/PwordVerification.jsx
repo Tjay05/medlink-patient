00406bf7826830d816a81c57da9ea64c6d40c333
Olatayo Tijesu <tjayolatayo@gmail.com>
2023-08-15 11:40:41 +0100

    Edit password verification page

--- src/pages/signup-login/PwordVerification.jsx
+++ src/pages/signup-login/PwordVerification.jsx
@@ -1,7 +1,33 @@
 import { useNavigate } from "react-router-dom";
+import { useState, useRef } from "react";
 
 const PwordVerification = () => {
   const history = useNavigate();
+  const [otpPassword, setOtpPassword] = useState("");
+  const inputRefs = useRef([]);
+
+  const handleChange = (index, value) => {
+    if (value.length > 1) {
+      value = value.slice(0, 1);
+    }
+
+    setOtpPassword((prevOtpPassword) => {
+      const newOtpPassword = prevOtpPassword.split("");
+      newOtpPassword[index] = value;
+      return newOtpPassword.join("");
+    });
+
+    if (value && index < inputRefs.current.length - 1) {
+      inputRefs.current[index + 1].focus();
+    }
+  };
+
+  const handleKeyDown = (index, event) => {
+    if (event.key === "Backspace" && !otpPassword[index] && index > 0) {
+      inputRefs.current[index - 1].focus();
+    }
+  };
+
   const handleClick = () => {
     history('/NewPassword')
   }
@@ -18,10 +44,17 @@ const PwordVerification = () => {
         <p>Enter the <span className="OTP">code</span> sent to your email to change your Password.</p>
         <form className="verify">
           <div className="otp">
-            <input type="text" id="" />
-            <input type="text" id="" />
-            <input type="text" id="" />
-            <input type="text" id="" />
+            {Array.from({ length: 4 }, (_, index) => (
+              <input
+                key={index}
+                type="number"
+                maxLength="1"
+                value={otpPassword[index] || ""}
+                onChange={(e) => handleChange(index, e.target.value)}
+                onKeyDown={(e) => handleKeyDown(index, e)}
+                ref={(ref) => (inputRefs.current[index] = ref)}
+              />
+            ))}
           </div>
           <button onClick={handleClick} >Next</button>
         </form>
