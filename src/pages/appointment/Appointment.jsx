ed7775d6604ed9104b6e9ccdce3a887ba0491d58
Olatayo Tijesu <tjayolatayo@gmail.com>
2023-08-21 14:14:56 +0100

    Add value to specialist for api consumption

--- src/pages/appointment/Appointment.jsx
+++ src/pages/appointment/Appointment.jsx
@@ -13,68 +13,116 @@ import opthalmology from '../../assets/icons/opthalmology.svg';
 import cardiolody from '../../assets/icons/heartDoc.svg';
 import nephrology from '../../assets/icons/nephrology.svg';
 import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { AppContext } from '../../layout/AppWrap';
 
 const Appointment = () => {
+  const { specialistType, setSpecialistType } = useContext(AppContext);
+
   const history = useNavigate();
-  const handleClick = () => {
-    history('PickDate');
+  const handleClick = (type) => {
+    setSpecialistType(type)
+    // history('PickDate');
+    console.log(specialistType);
   }
 
   return ( 
     <div className="appBodyWrapper">
-      <div onClick={handleClick} className='specialist'>
+      <div
+       onClick={() => handleClick('General-Practitioner')} 
+       className='specialist'
+      >
         <img src={generalDoc} alt="General Doctor" />
         <p>General Practitioner</p>
       </div>
-      <div onClick={handleClick} className='specialist'>
+      <div 
+        onClick={() => handleClick('Pediatrician')} 
+        className='specialist'
+      >
         <img src={pediatric} alt="Pediatrician" />
         <p>Pediatrics</p>
       </div>
-      <div onClick={handleClick} className='specialist'>
+      <div 
+        onClick={() => handleClick('Gynaecologist')} 
+        className='specialist'
+      >
         <img src={gynaecologist} alt="Gynaecologist" />
         <p>Gynaecologist</p>
       </div>
-      <div onClick={handleClick} className='specialist'>
+      <div 
+        onClick={() => handleClick('Orthopaedist')} 
+        className='specialist'
+      >
         <img src={orthopaedics} alt="Orthopaedician" />
         <p>Orthopaedics</p>
       </div>
-      <div onClick={handleClick} className='specialist'>
+      <div 
+        onClick={() => handleClick('Gastroenterologist')} 
+        className='specialist'
+      >
         <img src={gastro} alt="Gastroentologist" />
         <p>Gastroentologist</p>
       </div>
-      <div onClick={handleClick} className='specialist'>
+      <div 
+        onClick={() => handleClick('Urologist')} 
+        className='specialist'
+      >
         <img src={urologist} alt="Urologist" />
         <p>Urologist</p>
       </div>
-      <div onClick={handleClick} className='specialist'>
+      <div 
+        onClick={() => handleClick('Oncologist')} 
+        className='specialist'
+      >
         <img src={oncology} alt="Oncologist" />
         <p>Oncologist</p>
       </div>
-      <div onClick={handleClick} className='specialist'>
+      <div 
+        onClick={() => handleClick('Pulmonologist')} 
+        className='specialist'
+      >
         <img src={pulmonologist} alt="Pulmonologist" />
         <p>Pulmonologist</p>
       </div>
-      <div onClick={handleClick} className='specialist'>
+      <div 
+        onClick={() => handleClick('Physical Therapist')} 
+        className='specialist'
+      >
         <img src={physical_therapy} alt="Physical Therapist" />
         <p>Psychiatrist</p>
       </div>
-      <div onClick={handleClick} className='specialist'>
+      <div 
+        onClick={() => handleClick('Endocrinologist')} 
+        className='specialist'
+      >
         <img src={endocrinologist} alt="Endocrinologist" />
         <p>Endocrinologist</p>
       </div>
-      <div onClick={handleClick} className='specialist'>
+      <div 
+        onClick={() => handleClick('Dentist')} 
+        className='specialist'
+      >
         <img src={dentist} alt="Dentist" />
         <p>Dentist</p>
       </div>
-      <div onClick={handleClick} className='specialist'>
+      <div 
+        onClick={() => handleClick('Ophthalmologist')} 
+        className='specialist'
+      >
         <img src={opthalmology} alt="Opthalmologist" />
         <p>Opthalmologist</p>
       </div>
-      <div onClick={handleClick} className='specialist'>
+      <div 
+        onClick={() => handleClick('Cardiologist')} 
+        className='specialist'
+      >
         <img src={cardiolody} alt="Cardiologist" />
         <p>Cardiologist</p>
       </div>
-      <div onClick={handleClick} className='specialist'>
+      <div 
+        onClick={() => handleClick('Nephrologist')} 
+        className='specialist'
+      >
         <img src={nephrology} alt="Nephrologist" />
         <p>Nephrologist</p>
       </div>
