f1f9d5f51e55456c7abd63738573aa22c13a54f9
Olatayo Tijesu <tjayolatayo@gmail.com>
2023-08-22 12:36:00 +0100

    Edit logo

--- src/pages/signup-login/Login.jsx
+++ src/pages/signup-login/Login.jsx
@@ -60,7 +60,7 @@ const Login = () => {
     <div className="container loginPage">
       <header>
         <nav>
-          <p><span className="white-line">Me</span>dli<span className="red-line">nk</span></p> 
+          <p><span><div className="white-line"></div>Me</span>dli<span> <div className="red-line"></div>nk</span></p> 
         </nav>
       </header>
       <main>
