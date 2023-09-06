54474233fb4e8086b4644bebc25e16ed57282ee7
Olatayo Tijesu <tjayolatayo@gmail.com>
2023-09-06 22:06:49 +0100

    Comment import for link from pword change

--- src/pages/signup-login/NewPword.jsx
+++ src/pages/signup-login/NewPword.jsx
@@ -1,6 +1,6 @@
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+// import { Link, useNavigate } from "react-router-dom";
 
 const NewPword = () => {
   const [showPassword, setShowPassword] = useState(false);
